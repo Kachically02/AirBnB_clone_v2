7aa5c63b28edb1aaf3dcd174ebd9c83d0a859127
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-12-19 22:40:33 +0300

    setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,9 @@
--- script that prepares a dev MySQL server for the AirBnB clone project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- a new user hbnb_test (in localhost).
--- the password of hbnb_test should be set to hbnb_test_pwd.
+-- a n_ew user hbnb_test (in local_host).
+-- the pass_word of hbnb_test should be set to hbnb_test_pwd.
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
--- hbnb_test should have all privileges on the database hbnb_test_db only.
+-- hbnb_test should have all priv_ileges on the dat_abase hbnb_test_db on_y.
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
--- hbnb_test should have SELECT privilege on the database performance_schema.
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+-- hbnb_test sho_uld have SELECT priv__ilege on the data__base performance_schema__.
+GRANT SELECT ON `performance_schema'.* TO 'hbnb_test'@'localhost';
