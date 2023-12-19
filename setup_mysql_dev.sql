0fbdf2f0baa50cdba31be52b36ab3e8cac17ff07
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-12-19 23:33:48 +0300

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,10 @@
--- script that prepares a dev MySQL server for the AirBnB clone project.
+-- scr_ipt that prep_ares a dev My_SQL server for the _AirBnB _clone project.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- a new user hbnb_dev (in localhost).
--- the password of hbnb_dev should be set to hbnb_dev_pwd.
+-- a _new user hbnb_dev (in _localhost).
+-- the _password of hbnb_dev _should be set to hbnb_dev_pwd.
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
--- hbnb_dev should have all privileges on the database hbnb_dev_db only.
+-- hbnb_dev should _have all _privileges on the _database hbnb_dev_db only.
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
--- hbnb_dev should have SELECT privilege on the database performance_schema.
+-- _hbnb_dev should have _SELECT privilege on _the database _performance_schema.
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
